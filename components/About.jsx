130a1245683044d117890809212b496c87e0fe3f
Mustaphabn <110203644+Mustaphabn@users.noreply.github.com>
2023-01-28 13:56:42 +0100

    Update About.jsx

--- components/About.jsx
+++ components/About.jsx
@@ -2,7 +2,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
-import Mypic from '/../public/assets/mypic.png';
+import Mypic from '../public/assets/mypic.png';
 
 
 function About() {
